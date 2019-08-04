c219112d69aad7e731f410cfda103757cf5faa43
Rupinder Singh <>
2019-08-04 12:32:37 +0530

    Update development status to `5 - Production/Stable`

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md')) as f:
 setup(
     name = 'ln2sqlmodule',
     packages = ['ln2sqlmodule'],
-    version = 'v1.0.0',
+    version = 'v1.0.1',
     license='gpl-3.0',
     description = 'This is a wrapper around ln2sql by Jeremy Ferrero for convenient use',
     long_description=long_description,
@@ -16,11 +16,11 @@ setup(
     author = 'Rupinder Singh',
     author_email = 'rupinder1133@gmail.com',
     url = 'https://github.com/rupinder1133/ln2sqlmodule',
-    download_url = 'https://github.com/rupinder1133/ln2sqlmodule/archive/v1.0.0.tar.gz',
+    download_url = 'https://github.com/rupinder1133/ln2sqlmodule/archive/v1.0.1.tar.gz',
     keywords = ['ln2sql', 'NLP', 'SQL'],
     install_requires=[],
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
