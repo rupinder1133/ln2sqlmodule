3d0a848b6eeb6b594bab70e6c9fc8a3c61262d43
Rupinder Singh <>
2019-08-04 12:01:40 +0530

    Enhance setup.py to show readme on PyPi

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from distutils.core import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name = 'ln2sqlmodule',
     packages = ['ln2sqlmodule'],
-    version = '0.0.1',
+    version = 'v0.0.2',
     license='gpl-3.0',
     description = 'This is a wrapper around ln2sql by Jeremy Ferrero for convenient use',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author = 'Rupinder Singh',
     author_email = 'rupinder1133@gmail.com',
-    url = 'https://github.com/rupinder1133',
-    download_url = 'https://github.com/rupinder1133/ln2sqlmodule/archive/v0.0.1.tar.gz',
+    url = 'https://github.com/rupinder1133/ln2sqlmodule',
+    download_url = 'https://github.com/rupinder1133/ln2sqlmodule/archive/v0.0.2.tar.gz',
     keywords = ['ln2sql', 'NLP', 'SQL'],
     install_requires=[],
     classifiers=[
@@ -16,7 +24,6 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 2',
     ],
 )
