5d7a29ed6df81cc1d20c19f0779140d1fbb19c5e
Rupinder Singh <>
2019-08-20 23:16:32 +0530

    Include data files in package * used for language processing

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md')) as f:
 setup(
     name = 'ln2sqlmodule',
     packages = ['ln2sqlmodule'],
-    version = 'v1.0.1',
+    version = 'v1.0.2',
     license='gpl-3.0',
     description = 'This is a wrapper around ln2sql by Jeremy Ferrero for convenient use',
     long_description=long_description,
@@ -16,7 +16,7 @@ setup(
     author = 'Rupinder Singh',
     author_email = 'rupinder1133@gmail.com',
     url = 'https://github.com/rupinder1133/ln2sqlmodule',
-    download_url = 'https://github.com/rupinder1133/ln2sqlmodule/archive/v1.0.1.tar.gz',
+    download_url = 'https://github.com/rupinder1133/ln2sqlmodule/archive/v1.0.2.tar.gz',
     keywords = ['ln2sql', 'NLP', 'SQL'],
     install_requires=[],
     classifiers=[
@@ -26,4 +26,7 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Programming Language :: Python :: 2',
     ],
+    package_data={
+        '': ['stopwords/*.txt', 'lang/*.csv', 'thesaurus/*.dat'],
+    },
 )
